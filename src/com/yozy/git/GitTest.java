fafab53a2dd26a899dc4b2bbfecc04d5c7137a67
Hy9e <100647597+Hy9e@users.noreply.github.com>
2022-03-05 13:09:14 +0800

    Update GitTest.java

--- src/com/yozy/git/GitTest.java
+++ src/com/yozy/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Hello 4!");
         System.out.println("push test");
         System.out.println("push test SSH");
+        System.out.println("pull test");
     }
 }
